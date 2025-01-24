6069395000c58ca23e7c45463e92d419a0a32df0
mucyochris47 <mucyochris250@gmail.com>
2025-01-24 01:04:17 -0800

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,36 @@
 #!/usr/bin/env bash
-# script that sets up web servers for the deployment of web_static
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
+# a Bash script that sets up your web servers for the deployment of web_static
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
+sudo apt-get update -y
+sudo apt-get install -y nginx
+
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
-
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
-
+sudo chown -R ubuntu:ubuntu /data/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo printf %s "server {
+    listen 80;
+    listen [::]:80 default_server;
+
+    add_header X-Served-By $HOSTNAME;
+
+    root   /var/www/html;
+    index  index.html index.htm;
+    rewrite ^/redirect_me https://www.youtube.com/@cryptotechcoder permanent;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+	index index.html index.htm;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" | sudo tee /etc/nginx/sites-available/default > /dev/null
 
 sudo service nginx restart
--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-0x02. AirBnB clone - MySQL
-=======
-AirBnB_clone_v2 project
->>>>>>> d7e1ee852247afbc6d45427ddf930ec43db287c6
+0x03. AirBnB clone - Deploy static
